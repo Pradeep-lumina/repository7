8833384776613e4197958f04838e1223583de624
FITA <jafferali201211@gmail.com>
2021-03-16 09:53:56 +0530

    First Commit

---
+++ Batch10/src/automation/Basics.java
@@ -0,0 +1,19 @@
+package automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\FITA\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver(); 
+		
+		
+
+	}
+
+}
